10bb10052741530601149e223b2bdf2ce3a80765
Joel Stanley <stanleyj@live.ca>
2016-04-13 10:51:48 -0300

    Added GoFListAdapter.
    Provides an adapter between GoFList and ArrayList.

---
+++ CS4015Project/src/project/GoFListAdapter.java
@@ -0,0 +1,113 @@
+package project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jstanley
+ * Provides an adapter between GoFList and an ArrayList.
+ */
+public class GoFListAdapter implements GoFList {
+
+	/**
+	 * Holds a reference to the Adaptee.
+	 */
+	private List list;
+	
+	/**
+	 * Initializes list to be a new ArrayList.
+	 */
+	public GoFListAdapter() {
+		this.list = new ArrayList();
+	}
+	
+	/* (non-Javadoc)
+	 * @see project.GoFList#count()
+	 */
+	@Override
+	public int count() {
+		return this.list.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#getAt(int)
+	 */
+	@Override
+	public Object getAt(int index) {
+		return this.list.get(index);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#first()
+	 */
+	@Override
+	public Object first() {
+		return this.list.get(0);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#last()
+	 */
+	@Override
+	public Object last() {
+		return this.list.get(this.list.size() - 1);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#include(java.lang.Object)
+	 */
+	@Override
+	public boolean include(Object x) {
+		return this.list.contains(x);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#append(java.lang.Object)
+	 */
+	@Override
+	public void append(Object x) {
+		this.list.add(x);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#prepend(java.lang.Object)
+	 */
+	@Override
+	public void prepend(Object x) {
+		this.list.add(0, x);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#delete(java.lang.Object)
+	 */
+	@Override
+	public void delete(Object x) {
+		this.list.remove(x);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#deleteFirst()
+	 */
+	@Override
+	public void deleteFirst() {
+		this.list.remove(0);
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#deleteAll()
+	 */
+	@Override
+	public void deleteAll() {
+		this.list.clear();
+	}
+
+	/* (non-Javadoc)
+	 * @see project.GoFList#replace(int, java.lang.Object)
+	 */
+	@Override
+	public void replace(int index, Object x) {
+		this.list.remove(index);
+		this.list.add(index, x);
+	}
+
+}
