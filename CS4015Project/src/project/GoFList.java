0ef45fb014a24a9c1445c77dc866f59856ba088f
Joel Stanley <stanleyj@live.ca>
2016-04-13 10:05:19 -0300

    Added a GoFList interface.
    Will be implemented by the Row and Column classes.

---
+++ CS4015Project/src/project/GoFList.java
@@ -0,0 +1,75 @@
+package project;
+
+/**
+ * @author jstanley
+ * Defines a interface for a list data structure. 
+ */
+public interface GoFList {
+	
+	/**
+	 * Returns the number of elements in the list.
+	 * @return Number of elements in the list.
+	 */
+	public int count();
+	
+	/**
+	 * Returns the element at the given index.
+	 * @param index The index of the element to be returned.
+	 * @return The element at the given index.
+	 */
+	public Object getAt(int index);
+	
+	/**
+	 * Returns the first element of the list.
+	 * @return The first element of the list.
+	 */
+	public Object first();
+	
+	/**
+	 * Returns the last element of the list
+	 * @return The last element of the list.
+	 */
+	public Object last();
+	
+	/**
+	 * Returns whether or not x is in the list.
+	 * @param x The element to be checked for.
+	 * @return Whether or not x is in the list.
+	 */
+	public boolean include(Object x);
+	
+	/**
+	 * Adds x to the end of the list.
+	 * @param x The element to be added.
+	 */
+	public void append(Object x);
+	
+	/**
+	 * Adds x to the beginning of the list.
+	 * @param x The element to be added.
+	 */
+	public void prepend(Object x);
+	
+	/**
+	 * Removes the first appearance of x from the list.
+	 * @param x The element to be removed.
+	 */
+	public void delete(Object x);
+	
+	/**
+	 * Removes the first element from the list.
+	 */
+	public void deleteFirst();
+	
+	/**
+	 * Removes all the elements from the list.
+	 */
+	public void deleteAll();
+	
+	/**
+	 * Replaces the element at the given index by x.
+	 * @param index The index of the element to be replaced.
+	 * @param x The element that will be added to the list.
+	 */
+	public void replace(int index, Object x);
+}
